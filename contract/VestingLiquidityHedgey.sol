1990f7d7b13f0403a5eea6dd7d47976922c7483a
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-02-03 18:13:13 -0300

    update: automatedWithdrawalToPool

--- contract/VestingLiquidityHedgey.sol
+++ contract/VestingLiquidityHedgey.sol
@@ -4,31 +4,34 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/security/ReentrancyGuard.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/security/Pausable.sol";
 
-contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
-    address public poolAddress;
+contract LiquidityPoolInteractor is Ownable, ReentrancyGuard, Pausable {
+    address public liquidityPoolAddress;
     IERC20 public wrappedToken;
-    
+
     uint256 public blockCounter;
-    uint256 public transferInterval = 100;
+    uint256 public transferInterval = 200;
+    uint256 public maxAllowance;
 
     event TokensDeposited(address indexed from, uint256 amount);
     event ContractApproved(address indexed sender, uint256 amount);
     event TokensWithdrawn(address indexed to, uint256 amount);
+    event MaxAllowanceChanged(uint256 newMaxAllowance);
 
-    constructor(address _poolAddress, address _wETH) {
-        require(_poolAddress != address(0), "Invalid pool address");
-        require(_wETH != address(0), "Invalid wrapped token address");
+    constructor(address _liquidityPoolAddress, address _wrappedToken, uint256 _maxAllowance) {
+        require(_liquidityPoolAddress != address(0), "Invalid Liquidity Pool address");
+        require(_wrappedToken != address(0), "Invalid wrapped token address");
 
-        poolAddress = _poolAddress;
-        wrappedToken = IERC20(_wETH);
-        
+        liquidityPoolAddress = _liquidityPoolAddress;
+        wrappedToken = IERC20(_wrappedToken);
+        maxAllowance = _maxAllowance;
+
         // Initialize the block counter
         blockCounter = block.number;
     }
 
-    // Function to deposit wrapped tokens and transfer to the pool
-    function depositAndTransfer(uint256 amount) external {
+    function depositAndTransfer(uint256 amount) external whenNotPaused {
         require(amount > 0, "Deposit amount must be greater than 0");
 
         // Ensure the sender has approved this contract to spend their wrapped tokens
@@ -37,25 +40,25 @@ contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
         // Transfer tokens from sender to this contract
         require(wrappedToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
 
-        // Transfer all deposited tokens to the pool immediately
-        executeTransferToPool();
+        // Check if the transfer interval has passed and execute transfer to the Liquidity Pool
+        if (block.number - blockCounter >= transferInterval) {
+            executeTransferToLiquidityPool();
+            // Reset the block counter
+            blockCounter = block.number;
+        }
 
-        // Optionally, you can perform additional logic or emit events here
-        
-        // Reset the block counter
-        blockCounter = block.number;
+        emit TokensDeposited(msg.sender, amount);
     }
 
-    // Approve the contract to spend a certain amount of wrapped tokens
-    function approveContract(uint256 amount) external onlyOwner {
+    function approveContract(uint256 amount) external onlyOwner whenNotPaused {
         // Approve this contract to spend the specified amount of tokens
+        require(amount <= maxAllowance, "Exceeds maximum allowance");
         wrappedToken.approve(address(this), amount);
 
         emit ContractApproved(msg.sender, amount);
     }
 
-    // Withdraw wrapped tokens from the contract (only callable by the owner)
-    function withdrawal(address to, uint256 amount) external onlyOwner nonReentrant {
+    function withdrawal(address to, uint256 amount) external onlyOwner nonReentrant whenNotPaused {
         require(to != address(0), "Invalid withdrawal address");
         require(amount > 0, "Withdrawal amount must be greater than 0");
         require(wrappedToken.balanceOf(address(this)) >= amount, "Insufficient balance");
@@ -66,19 +69,39 @@ contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
         emit TokensWithdrawn(to, amount);
     }
 
-    // Transfer ownership to a new owner (only callable by the current owner)
-    function transferOwnership(address newOwner) public onlyOwner override {
+    function transferOwnership(address newOwner) public override onlyOwner {
         require(newOwner != address(0), "New owner is the zero address");
-        emit OwnershipTransferred(owner(), newOwner);
-        _transferOwnership(newOwner);
+        super.transferOwnership(newOwner);
     }
-    
-    // Internal function to execute the transfer to the pool
-    function executeTransferToPool() internal {
-        // Transfer total tokens from this contract to the pool address
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function automatedWithdrawalToPool() external whenNotPaused {
+        // Check if the transfer interval has passed and execute transfer to the Liquidity Pool
+        if (block.number - blockCounter >= transferInterval) {
+            executeTransferToLiquidityPool();
+            // Reset the block counter
+            blockCounter = block.number;
+        }
+    }
+
+    // Internal function to execute the transfer to the Liquidity Pool
+    function executeTransferToLiquidityPool() internal {
+        // Transfer total tokens from this contract to the Liquidity Pool address
         uint256 contractBalance = wrappedToken.balanceOf(address(this));
         if (contractBalance > 0) {
-            require(wrappedToken.transfer(poolAddress, contractBalance), "Transfer to pool failed");
+            require(wrappedToken.transfer(liquidityPoolAddress, contractBalance), "Transfer to Liquidity Pool failed");
         }
+    }
+
+    function setMaxAllowance(uint256 newMaxAllowance) external onlyOwner {
+        maxAllowance = newMaxAllowance;
+        emit MaxAllowanceChanged(newMaxAllowance);
     }
 }
--- contract/VestingLiquidityHedgey_flattened.sol
+++ contract/VestingLiquidityHedgey_flattened.sol
@@ -106,6 +106,113 @@ abstract contract Context {
     }
 }
 
+// File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/security/Pausable.sol
+
+
+// OpenZeppelin Contracts (last updated v4.7.0) (security/Pausable.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @dev Contract module which allows children to implement an emergency stop
+ * mechanism that can be triggered by an authorized account.
+ *
+ * This module is used through inheritance. It will make available the
+ * modifiers `whenNotPaused` and `whenPaused`, which can be applied to
+ * the functions of your contract. Note that they will not be pausable by
+ * simply including this module, only once the modifiers are put in place.
+ */
+abstract contract Pausable is Context {
+    /**
+     * @dev Emitted when the pause is triggered by `account`.
+     */
+    event Paused(address account);
+
+    /**
+     * @dev Emitted when the pause is lifted by `account`.
+     */
+    event Unpaused(address account);
+
+    bool private _paused;
+
+    /**
+     * @dev Initializes the contract in unpaused state.
+     */
+    constructor() {
+        _paused = false;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is not paused.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+    modifier whenNotPaused() {
+        _requireNotPaused();
+        _;
+    }
+
+    /**
+     * @dev Modifier to make a function callable only when the contract is paused.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    modifier whenPaused() {
+        _requirePaused();
+        _;
+    }
+
+    /**
+     * @dev Returns true if the contract is paused, and false otherwise.
+     */
+    function paused() public view virtual returns (bool) {
+        return _paused;
+    }
+
+    /**
+     * @dev Throws if the contract is paused.
+     */
+    function _requireNotPaused() internal view virtual {
+        require(!paused(), "Pausable: paused");
+    }
+
+    /**
+     * @dev Throws if the contract is not paused.
+     */
+    function _requirePaused() internal view virtual {
+        require(paused(), "Pausable: not paused");
+    }
+
+    /**
+     * @dev Triggers stopped state.
+     *
+     * Requirements:
+     *
+     * - The contract must not be paused.
+     */
+    function _pause() internal virtual whenNotPaused {
+        _paused = true;
+        emit Paused(_msgSender());
+    }
+
+    /**
+     * @dev Returns to normal state.
+     *
+     * Requirements:
+     *
+     * - The contract must be paused.
+     */
+    function _unpause() internal virtual whenPaused {
+        _paused = false;
+        emit Unpaused(_msgSender());
+    }
+}
+
 // File: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol
 
 
@@ -280,30 +387,33 @@ pragma solidity ^0.8.0;
 
 
 
-contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
-    address public poolAddress;
+
+contract LiquidityPoolInteractor is Ownable, ReentrancyGuard, Pausable {
+    address public liquidityPoolAddress;
     IERC20 public wrappedToken;
-    
+
     uint256 public blockCounter;
-    uint256 public transferInterval = 100;
+    uint256 public transferInterval = 200;
+    uint256 public maxAllowance;
 
     event TokensDeposited(address indexed from, uint256 amount);
     event ContractApproved(address indexed sender, uint256 amount);
     event TokensWithdrawn(address indexed to, uint256 amount);
+    event MaxAllowanceChanged(uint256 newMaxAllowance);
 
-    constructor(address _poolAddress, address _wETH) {
-        require(_poolAddress != address(0), "Invalid pool address");
-        require(_wETH != address(0), "Invalid wrapped token address");
+    constructor(address _liquidityPoolAddress, address _wrappedToken, uint256 _maxAllowance) {
+        require(_liquidityPoolAddress != address(0), "Invalid Liquidity Pool address");
+        require(_wrappedToken != address(0), "Invalid wrapped token address");
 
-        poolAddress = _poolAddress;
-        wrappedToken = IERC20(_wETH);
-        
+        liquidityPoolAddress = _liquidityPoolAddress;
+        wrappedToken = IERC20(_wrappedToken);
+        maxAllowance = _maxAllowance;
+
         // Initialize the block counter
         blockCounter = block.number;
     }
 
-    // Function to deposit wrapped tokens and transfer to the pool
-    function depositAndTransfer(uint256 amount) external {
+    function depositAndTransfer(uint256 amount) external whenNotPaused {
         require(amount > 0, "Deposit amount must be greater than 0");
 
         // Ensure the sender has approved this contract to spend their wrapped tokens
@@ -312,25 +422,25 @@ contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
         // Transfer tokens from sender to this contract
         require(wrappedToken.transferFrom(msg.sender, address(this), amount), "Transfer failed");
 
-        // Transfer all deposited tokens to the pool immediately
-        executeTransferToPool();
+        // Check if the transfer interval has passed and execute transfer to the Liquidity Pool
+        if (block.number - blockCounter >= transferInterval) {
+            executeTransferToLiquidityPool();
+            // Reset the block counter
+            blockCounter = block.number;
+        }
 
-        // Optionally, you can perform additional logic or emit events here
-        
-        // Reset the block counter
-        blockCounter = block.number;
+        emit TokensDeposited(msg.sender, amount);
     }
 
-    // Approve the contract to spend a certain amount of wrapped tokens
-    function approveContract(uint256 amount) external onlyOwner {
+    function approveContract(uint256 amount) external onlyOwner whenNotPaused {
         // Approve this contract to spend the specified amount of tokens
+        require(amount <= maxAllowance, "Exceeds maximum allowance");
         wrappedToken.approve(address(this), amount);
 
         emit ContractApproved(msg.sender, amount);
     }
 
-    // Withdraw wrapped tokens from the contract (only callable by the owner)
-    function withdrawal(address to, uint256 amount) external onlyOwner nonReentrant {
+    function withdrawal(address to, uint256 amount) external onlyOwner nonReentrant whenNotPaused {
         require(to != address(0), "Invalid withdrawal address");
         require(amount > 0, "Withdrawal amount must be greater than 0");
         require(wrappedToken.balanceOf(address(this)) >= amount, "Insufficient balance");
@@ -341,19 +451,39 @@ contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
         emit TokensWithdrawn(to, amount);
     }
 
-    // Transfer ownership to a new owner (only callable by the current owner)
-    function transferOwnership(address newOwner) public onlyOwner override {
+    function transferOwnership(address newOwner) public override onlyOwner {
         require(newOwner != address(0), "New owner is the zero address");
-        emit OwnershipTransferred(owner(), newOwner);
-        _transferOwnership(newOwner);
+        super.transferOwnership(newOwner);
     }
-    
-    // Internal function to execute the transfer to the pool
-    function executeTransferToPool() internal {
-        // Transfer total tokens from this contract to the pool address
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function automatedWithdrawalToPool() external whenNotPaused {
+        // Check if the transfer interval has passed and execute transfer to the Liquidity Pool
+        if (block.number - blockCounter >= transferInterval) {
+            executeTransferToLiquidityPool();
+            // Reset the block counter
+            blockCounter = block.number;
+        }
+    }
+
+    // Internal function to execute the transfer to the Liquidity Pool
+    function executeTransferToLiquidityPool() internal {
+        // Transfer total tokens from this contract to the Liquidity Pool address
         uint256 contractBalance = wrappedToken.balanceOf(address(this));
         if (contractBalance > 0) {
-            require(wrappedToken.transfer(poolAddress, contractBalance), "Transfer to pool failed");
+            require(wrappedToken.transfer(liquidityPoolAddress, contractBalance), "Transfer to Liquidity Pool failed");
         }
+    }
+
+    function setMaxAllowance(uint256 newMaxAllowance) external onlyOwner {
+        maxAllowance = newMaxAllowance;
+        emit MaxAllowanceChanged(newMaxAllowance);
     }
 }
