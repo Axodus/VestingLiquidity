f23595e36054ee2eff11757d8441e663e895fede
Mauricio Zanetti Filho <contact@thinkincoin.com>
2024-02-03 12:15:43 -0300

    update: reentrancy and transferOwner

--- contract/VestingLiquidityHedgey.sol
+++ contract/VestingLiquidityHedgey.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/security/ReentrancyGuard.sol";
 
-contract VestingLiquidityHedgey is Ownable {
+contract VestingLiquidityHedgey is Ownable, ReentrancyGuard {
     address public receiverWallet;
     address public poolAddress;
     IERC20 public wrappedToken;
@@ -14,7 +15,6 @@ contract VestingLiquidityHedgey is Ownable {
 
     event TokensDeposited(address indexed from, uint256 amount);
     event ContractApproved(address indexed sender, uint256 amount);
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
     event TokensWithdrawn(address indexed to, uint256 amount);
 
     constructor(address _receiverWallet, address _poolAddress, address _wrappedToken) {
@@ -59,7 +59,7 @@ contract VestingLiquidityHedgey is Ownable {
     }
 
     // Withdraw wrapped tokens from the contract (only callable by the owner)
-    function withdrawal(address to, uint256 amount) external onlyOwner {
+    function withdrawal(address to, uint256 amount) external onlyOwner nonReentrant {
         require(to != address(0), "Invalid withdrawal address");
         require(amount > 0, "Withdrawal amount must be greater than 0");
         require(wrappedToken.balanceOf(address(this)) >= amount, "Insufficient balance");
@@ -71,7 +71,7 @@ contract VestingLiquidityHedgey is Ownable {
     }
 
     // Transfer ownership to a new owner (only callable by the current owner)
-    function transferOwnership(address newOwner) external onlyOwner {
+    function transferOwnership(address newOwner) public onlyOwner override {
         require(newOwner != address(0), "New owner is the zero address");
         emit OwnershipTransferred(owner(), newOwner);
         _transferOwnership(newOwner);
